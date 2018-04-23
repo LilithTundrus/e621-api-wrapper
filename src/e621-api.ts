d7f23a7f88587095bf205495ad60c06106163647
Lilith Tundrus <echofox123@gmail.com>
2018-04-23 19:39:58 -0400

    modifying this class to better reflect the new structures being implemented

--- src/e621-api.ts
+++ src/e621-api.ts
@@ -1,20 +1,21 @@
 import * as request from 'request'
 import { generateAPIKeyURL, requestUrl } from './utils';
+import { subClassOptions } from './interfaces'
 
 import Tags from './subComponents/Tags';
 import Posts from './subComponents/Posts';
+import { RequestServices } from './RequestService';
 
 declare const Promise: any;
 
 // TODO: Fill in ALL API endpoints
 // TODO: Document all of the class endpoints so the user knows what they do
-// TODO: Get logins working
+// TODO: Get logins working in a better way
 
 export default class e621 {
     private userAgent: string;
-    private userName: string;
-    private apiKey: string;
     private pageLimit: number;
+    public requestservices: RequestServices;
     public tags: Tags;
     public posts: Posts;
 
@@ -23,40 +24,16 @@ export default class e621 {
         if (pageLimit) this.pageLimit = pageLimit;
         else this.pageLimit = 3;
         if (userName && apiKey) {
-            this.userName = userName;
-            this.apiKey = apiKey
+            this.requestservices = new RequestServices(userAgent, userName, apiKey);
+        } else {
+            // we won't send the RequestServices a username + hash if they are not given
+            this.requestservices = new RequestServices(userAgent);
         }
-        this.tags = new Tags(this.userAgent, this.pageLimit, this.userName, this.apiKey);
+        this.tags = new Tags(this.pageLimit, this.requestservices);
         this.posts = new Posts(this.userAgent, this.pageLimit);
-
-        // set the API key and username to be used with all requests/posts if one is given 
     }
 
     public get agent() { return this.userAgent; }
     public get limit() { return this.pageLimit; }
 
-    /** Used to get the api key
-     * 
-     * WARNING: This is dangerous to use without encryption and can lead to user data leakage. Only use via enrypted methods.
-     * @param {String} username e621 username
-     * @param {String} password password (only needed to generate the key)
-     */
-    public getApiKey(username: string, password: string): Promise<string> {
-        let userAgent = this.userAgent
-        return new Promise(function (resolve, reject) {
-            const url = generateAPIKeyURL(username, password);
-            requestUrl(url, userAgent)
-                .then((response) => {
-                    resolve(response.password_hash);
-                })
-                .catch((err) => {
-                    throw Error(err);
-                })
-        });
-    }
-
-    // this is how we allow a user to GET/POST as themselves
-    public setApiKey() {
-
-    }
 }
