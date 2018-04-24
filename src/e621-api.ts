2f35ca4345beb54e8556c8e74f5e80874428f4f4
Lilith Tundrus <echofox123@gmail.com>
2018-04-24 00:20:12 -0400

    Added basic documentation

--- src/e621-api.ts
+++ src/e621-api.ts
@@ -14,10 +14,19 @@ declare const Promise: any;
 export default class e621 {
     private userAgent: string;
     private pageLimit: number;
+    // these delcarations are how we attach new classes to properties
     public requestservices: RequestServices;
     public tags: Tags;
     public posts: Posts;
 
+    /**
+     * Creates an instance of e621.
+     * @param {string} userAgent This identifies you to the API
+     * @param {string} [userName] Your user name (optional, if making changes to data)
+     * @param {string} [apiKey] Your API key to use (optional, if making changes to data)
+     * @param {number} [pageLimit] Page limit for paginated calls from e621
+     * @memberof e621
+     */
     public constructor(userAgent: string, userName?: string, apiKey?: string, pageLimit?: number) {
         this.userAgent = userAgent;
         if (pageLimit) this.pageLimit = pageLimit;
@@ -28,11 +37,12 @@ export default class e621 {
             // we won't send the RequestServices a username + hash if they are not given
             this.requestservices = new RequestServices(userAgent);
         }
+        // assign the properties as new classes with the page limit and the methods for get/post 
+        // operations as arguments
         this.tags = new Tags(this.pageLimit, this.requestservices);
         this.posts = new Posts(this.pageLimit, this.requestservices);
     }
 
     public get agent() { return this.userAgent; }
     public get limit() { return this.pageLimit; }
-
 }
