eb794e65e553c70692a6cb24b4f390f702c142ce
Lilith Tundrus <echofox123@gmail.com>
2018-04-27 23:03:39 -0400

    minor fix

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -133,12 +133,11 @@ export interface e621CommentUpdateJSON {
 }
 
 export interface e621PoolInfo {
-     id: number,
-        name: string,
-        created_at: { json_class: 'Time', s: number, n: number },
-        updated_at: { json_class: 'Time', s: number, n: number },
-        user_id: number,
-        is_locked: boolean,
-        post_count: number,
-    
+    id: number,
+    name: string,
+    created_at: { json_class: 'Time', s: number, n: number },
+    updated_at: { json_class: 'Time', s: number, n: number },
+    user_id: number,
+    is_locked: boolean,
+    post_count: number,
 }
