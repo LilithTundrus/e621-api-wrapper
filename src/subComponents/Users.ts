c83c63f57a6650424b1759412e4f4de17d75234d
Lilith Tundrus <echofox123@gmail.com>
2018-04-29 21:51:59 -0400

    implemented listUsers, with 2 args. Anything else becomes too complicated and the get methods work better for single calls

--- src/subComponents/Users.ts
+++ src/subComponents/Users.ts
@@ -2,6 +2,7 @@ import { RequestServices } from '../RequestService';
 import {
     e621POSTResponse, e621UserInfo
 } from '../interfaces';
+import { e621UserLevels } from '../enums';
 
 export default class Users {
     private pageLimit: number;
@@ -12,55 +13,21 @@ export default class Users {
         this.requestServices = requestServices;
     }
 
-    listUsers() {
-        //         The base URL is /user/index.xml. If you don't specify any parameters you'll get a listing of all users.
-
-        // id The ID number of the user.
-        // name Text matching part or all of a user's name.
-        // level Permission level. Can be one of the following or use -1 for Any:
-        // 0 Unactivated
-        // 10 Blocked
-        // 20 Member
-        // 30 Privileged
-        // 33 Contributor
-        // 34 Former Staff
-        // 35 Janitor
-        // 40 Mod
-        // 50 Admin
-        // order Sort order. Can be one of the following:
-        // name Username, based on Unicode values (ascending)
-        // posts Uploaded post count (descending)
-        // deleted Deleted post count (descending)
-        // notes Note edit count (descending)
-        // tagedits Tag edit count (descending)
-        // date Join date from most recent to least
-        // record Cumulative user record score (descending)
-        // Returns:
-        // An array of users whose ID exactly matches the supplied id attribute or whose name contains the supplied name attribute. Each user element has the following attributes:
-
-        // name The name of the user.
-        // id The ID number of the user.
-        // level The permission level of the user. See list under level parameter above for possible values.
-        // created_at When the user created their account.
-        // avatar_id The ID number of the post the user is currently using as their avatar, if any.
-        // stats An XML child element or JSON object with the following attributes:
-        // post_count
-        // del_post_count
-        // edit_count The number of tag edits the user has made.
-        // favorite_count
-        // wiki_count
-        // forum_post_count
-        // note_count
-        // comment_count
-        // blip_count
-        // set_count
-        // pool_update_count
-        // pos_user_records
-        // neutral_user_records
-        // neg_user_records
-        // subscriptions User tag subscriptions, if any. In JSON responses this is represented by a subscriptions object. In XML responses it is represented by one or more subscription child elements.
-        // artist_tags Artist tags associated with the user. Represented by a child element in XML responses or an array in JSON responses.
+    /** List all users by a set of given options, if no arguments are passed, you will get a listing of all users
+     * @param {e621UserLevels} [level] Permission level, optional
+     * @param {string} [order] Order of the users, can be `name`, `posts`, `deleted`, `notes`, `tagedits, `date` or `record`
+     * @returns Promise<e621UserInfo[]>
+     * @memberof Users
+     */
+    listUsers(level?: e621UserLevels, order?: string) {
         let url = `https://e621.net/user/index.json?`;
+
+        // if no level set but order set
+        if (!level && order) url = url + `order=${order}`;
+        // if no order set but level is set
+        else if (!order && level) url = url + `level=${level}`;
+        // if both are set
+        else if (order && level) url = url + `level=${level}&order=${order}`;
 
         return this.requestServices.get(url)
             .then((response: e621UserInfo[]) => {
@@ -104,5 +71,4 @@ export default class Users {
                 throw Error(err);
             })
     }
-
 }
