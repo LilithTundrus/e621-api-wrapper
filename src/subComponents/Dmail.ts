b62cbc289bdf6595d10a0b14eb63ac79a87a3d19
Lilith Tundrus <echofox123@gmail.com>
2018-05-02 20:02:02 -0400

    all search endpoints should now work

--- src/subComponents/Dmail.ts
+++ src/subComponents/Dmail.ts
@@ -12,20 +12,6 @@ export default class Dmail {
         this.requestServices = requestServices;
     }
 
-    list() {
-        // The base URL is /dmail/inbox.xml. If you don't specify any parameters you'll get a list of the most recently sent and received dmails, excluding hidden dmails.
-
-        // from_name Returns dmails sent from the user with the given username.*
-        // to_name Returns dmails sent to the user with the given username.*
-        // title Returns dmails with titles which contain the given string.
-        // date_start Returns dmails sent on or after the given date. Format is yyyy-mm-dd.
-        // date_end Returns dmails sent before the given date. Format is yyyy-mm-dd.
-        // show Returns incoming dmails when set to in, outgoing dmails when set to out, or both when set to all. Defaults to the setting last used through the site's normal interface.
-        // visibility Returns hidden dmails when set to hidden, visible dmails when set to unhidden, or both when set to all.
-        // page Page number to retrieve in the paginated results.
-        // *If the inbox does not contain any dmails with the given username, this parameter will be ignored. Thus it is not safe to rely on this function to return dmails from/to specific users. The dmails returned should be checked individually for the desired senders/recipients.
-    }
-
     /** Get your Dmail inbox, will return an empty array if page number is empty
      * @param {number} [page] Page number to retrieve
      * @returns Array of dmail data
@@ -83,14 +69,21 @@ export default class Dmail {
             })
     }
 
-
+    /** Search your inbox for a message with a given `query` (exact match)
+     * 
+     * **NOTE**: you do NOT need to include the 'Re:' in your `query` string
+     * @param {string} query Exact message subject match
+     * @param {number} [page] Page number to retrieve
+     * @returns Array of dmail data that match your query
+     * @memberof Dmail
+     */
     searchInbox(query: string, page?: number) {
         let url = `https://e621.net/dmail/inbox.json?show=in&title=${query}`;
 
         if (page) url += `&page=${page}`;
 
         return this.requestServices.get(url)
-            .then((response: any) => {
+            .then((response: e621DmailMessage[]) => {
                 return response;
             })
             .catch((err) => {
@@ -98,13 +91,21 @@ export default class Dmail {
             })
     }
 
+    /** Search your outbox for a message with a given `query` (exact match)
+     * 
+     * **NOTE**: you do NOT need to include the 'Re:' in your `query` string
+     * @param {string} query Exact message subject match
+     * @param {number} [page] Page number to retrieve
+     * @returns Array of dmail data that match your query
+     * @memberof Dmail
+     */
     searchOutbox(query: string, page?: number) {
         let url = `https://e621.net/dmail/inbox.json?show=out&title=${query}`;
 
         if (page) url += `&page=${page}`;
 
         return this.requestServices.get(url)
-            .then((response: any) => {
+            .then((response: e621DmailMessage[]) => {
                 return response;
             })
             .catch((err) => {
@@ -112,13 +113,21 @@ export default class Dmail {
             })
     }
 
+    /** Search ALL of your mail for a message with a given `query` (exact match)
+     * 
+     * **NOTE**: you do NOT need to include the 'Re:' in your `query` string
+     * @param {string} query Exact message subject match
+     * @param {number} [page] Page number to retrieve
+     * @returns Array of dmail data that match your query
+     * @memberof Dmail
+     */
     searchAllMail(query: string, page?: number) {
         let url = `https://e621.net/dmail/inbox.json?show=all&title=${query}`;
 
         if (page) url += `&page=${page}`;
 
         return this.requestServices.get(url)
-            .then((response: any) => {
+            .then((response: e621DmailMessage[]) => {
                 return response;
             })
             .catch((err) => {
@@ -146,6 +155,18 @@ export default class Dmail {
         // The base URL is / dmail / mark_all_read.xml.
 
         // Marks all dmails in the inbox as read.
+    }
+
+    getHidden() {
+
+    }
+
+    showMail() {
+
+    }
+
+    hideMail() {
+
     }
 }
 
