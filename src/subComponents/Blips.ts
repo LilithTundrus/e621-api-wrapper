f225873cc5b356322a72dad409d285fdb05bb95d
Lilith Tundrus <echofox123@gmail.com>
2018-05-06 01:26:43 -0400

    First clean pass on blips

--- src/subComponents/Blips.ts
+++ src/subComponents/Blips.ts
@@ -14,14 +14,14 @@ export default class Blips {
 
     /** Create a blip, either in response to another blip or standalone
      * @param {string} bodyText Text of the blip's body to create
-     * @param {number} [responseID] ID of another blip to respond to
+     * @param {(number | string)} [responseID] ID of another blip to respond to
      * @returns Promise<e621POSTResponse>
      * @memberof Blips
      */
-    public create(bodyText: string, responseID?: number) {
+    public create(bodyText: string, responseID?: number | string) {
         let url = `https://e621.net/blip/create.json`;
-        let postObj;
 
+        let postObj;
         if (!responseID) {
             postObj = {
                 "blip[body]": bodyText,
@@ -43,12 +43,12 @@ export default class Blips {
     }
 
     /** Update a blip's body by ID
-     * @param {number} blipID ID of the blip to update
+     * @param {(number | string)} blipID ID of the blip to update
      * @param {string} bodyText New text of the blip body
      * @returns Promise<e621POSTResponse>
      * @memberof Blips
      */
-    public update(blipID: number, bodyText: string) {
+    public update(blipID: number | string, bodyText: string) {
         let url = `https://e621.net/blip/update.json`;
 
         let postObj = {
@@ -66,12 +66,13 @@ export default class Blips {
     }
 
     /** Get a set of recent blips
-     * @param {number} [page] 
+     * @param {number} [page] Page number to return
      * @returns Promise<e621BlipInfo[]>
      * @memberof Blips
      */
     public getRecentBlips(page?: number) {
         let url = `https://e621.net/blip/index.json?`;
+
         if (page) url += `page=${page}`;
 
         return this.requestServices.get(url)
@@ -84,12 +85,14 @@ export default class Blips {
     }
 
     /** Get a blip's information by ID, returns a single blip
-     * @param {number} blipID ID for the blip to retrieve
+     * @param {(number | string)} ID for the blip to retrieve
      * @returns Promise<e621BlipInfo>
      * @memberof Blips
      */
-    public getBlipByID(blipID: number) {
-        return this.requestServices.get(`https://e621.net/blip/show.json?id=${blipID}`)
+    public getBlipByID(blipID: number | string) {
+        let url = `https://e621.net/blip/show.json?id=${blipID}`;
+
+        return this.requestServices.get(url)
             .then((response: e621BlipInfo) => {
                 return response;
             })
@@ -99,11 +102,12 @@ export default class Blips {
     }
 
     /** Get a blip's responses (if any). If an error is thrown, then their are no responses for the first (or given) page
-     * @param {number} blipID ID for the blip to retrieve the responses ofs
+     * @param {(number | string)} blipID ID for the blip to retrieve the responses of
+     * @param {number} [page] Page number to return
      * @returns Promise<e621BlipInfo[]>
      * @memberof Blips
      */
-    public getBlipResponses(blipID: number, page?: number) {
+    public getBlipResponses(blipID: number | string, page?: number) {
         let url = `https://e621.net/blip/index.json?response_to=${blipID}`;
         if (page) url += `page=${page}`;
 
@@ -116,9 +120,9 @@ export default class Blips {
             })
     }
 
-    /** Search for a blip that contains a given `textToMatch`
-     * @param {string} textToMatch 
-     * @param {number} [page] 
+    /** Search for a blip that contains a given `textToMatch` string
+     * @param {string} textToMatch String to search for matching blips
+     * @param {number} [page] Page number to return
      * @returns Promise<e621BlipInfo[]>
      * @memberof Blips
      */
